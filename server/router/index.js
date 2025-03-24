2b2371cc43b8f661d5e344fe9c85bc9d6f1f513f
Simar Narula <163547703+simar111@users.noreply.github.com>
2025-03-24 09:46:46 +0530

    done

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const Ccarwash=require('../controller/carsignup');
 const Ccarlogin=require('../controller/carlogin');
-const Cusersignup = require('../Controller/usersignup')
+// const Cusersignup = require('../Controller/usersignup')
 const Ccarbooking=require('../Controller/userbooking')
 const booking = require('../Controller/booking')
 //post api for car sign up page
@@ -10,22 +10,22 @@ router.get('/getcarsignup',Ccarwash.getuserdata)
 router.post('/carsignup',Ccarwash.insertdata)
 //post api for car login page
 router.post('/carlogin',Ccarlogin.insertuserdata)
-router.post('/getusersignup',Cusersignup.insertdata)
-router.post('/getuserlogin',Cusersignup.loginUser)
+// router.post('/getusersignup',Cusersignup.insertdata)
+// router.post('/getuserlogin',Cusersignup.loginUser)
 //car booking apis
 router.post('/insertbooking',Ccarbooking.bookCarService)
 //api for user profile edit
-router.get('/getuserdata',Cusersignup.getUserByEmail)
+// router.get('/getuserdata',Cusersignup.getUserByEmail)
 //api for showing booking details to user
 router.get('/getuserbooking',Ccarbooking.getBookingByEmail)
 //api for updating the profile of the user
-router.put('/updateuser',Cusersignup.updateUserProfile)
+// router.put('/updateuser',Cusersignup.updateUserProfile)
 //api for contact us page 
 router.post('/usercontactus', Ccarbooking.contactUs)
 //api to get all user data in admin panel
-router.get('/getalluserdata',Cusersignup.getuserdata)
+// router.get('/getalluserdata',Cusersignup.getuserdata)
 //api to delete user in admin panel
-router.delete('/deleteuser/:email',Cusersignup.deleteuser)
+// router.delete('/deleteuser/:email',Cusersignup.deleteuser)
 //api to get all booking details in admin pannel
 router.get('/getallbooking',Ccarbooking.getAllBookings)
 //api to get contact us details
